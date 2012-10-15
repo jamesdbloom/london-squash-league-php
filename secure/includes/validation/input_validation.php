d49925c2f4168592823af2cb5a5c1f51a513279f
James Bloom <jamesdbloom@gmail.com>
2012-10-15 13:27:50 +0100

    added input validation for email and string using filters

--- secure/includes/validation/input_validation.php
+++ secure/includes/validation/input_validation.php
@@ -3,21 +3,21 @@ class InputValidation
 {
     public static function clean_input($input)
     {
-        // todo
-        return stripslashes($input);
+        return filter_var(stripslashes($input), FILTER_SANITIZE_STRING);
     }
 
     public static function is_valid_email_address($email)
     {
+        $sanitized_email = filter_var($email, FILTER_SANITIZE_EMAIL);
         // First, we check that there's one @ symbol,
         // and that the lengths are right.
-        if (!ereg("^[^@]{1,64}@[^@]{1,255}$", $email)) {
+        if (!ereg("^[^@]{1,64}@[^@]{1,255}$", $sanitized_email)) {
             // Email invalid because wrong number of characters
             // in one section or wrong number of @ symbols.
             return false;
         }
         // Split it into sections to make life easier
-        $email_array = explode("@", $email);
+        $email_array = explode("@", $sanitized_email);
         $local_array = explode(".", $email_array[0]);
         for ($i = 0; $i < sizeof($local_array); $i++) {
             if
