80a7dd2c051edb1b7da8f3828099bebd5014cdcd
James Bloom <jamesdbloom@gmail.com>
2012-10-31 22:12:54 +0000

    fixing issue with tel form field not appearing like other form fields

--- includes/html/page.php
+++ includes/html/page.php
@@ -12,7 +12,7 @@ class Page
         print "<!DOCTYPE>";
         print "<html xmlns='http://www.w3.org/1999/xhtml' lang='en'>";
         print "<head>";
-        array_unshift($css_urls, Link::root . 'view/global_2012_08_31_09_42.css');
+        array_unshift($css_urls, Link::root . 'view/global_2012_08_31_22_10.css');
         print "<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>";
         print "<meta name='format-detection' content='telephone=no'>";
         print "<meta name='viewport' content='width=device-width, initial-scale=1.0'>";
